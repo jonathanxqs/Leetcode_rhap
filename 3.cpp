2f406862892572a07f6e79a1397d2872be04bcea
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-13 15:10:11 -0400

    Solving 3 O(n) solution

--- 3.cpp
+++ 3.cpp
@@ -5,16 +5,18 @@ public:
     int lengthOfLongestSubstring(string s) {
 
         int n = s.length(), ans = 0;
-        
+        // cout << n <<endl;
         map<char, int> map; // current index of character
         
         int j=0,i=0;
         // try to extend the range [i, j]
+
         for (j = 0, i = 0; j < n; j++) {
+        	cout<<i<<" "<<j<<endl;
             if (map.find(s[j]) !=map.end()) {
                 i = max(map[s[j]], i);
             }
-            
+
             ans = max(ans, j - i + 1);
             map[s[j+1]]= j + 1;
         }
