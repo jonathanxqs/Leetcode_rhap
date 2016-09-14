7d9cd633af54bd2e189651393991f2470f53e94e
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-14 16:44:13 -0400

    Debugging 3

--- 3.cpp
+++ 3.cpp
@@ -12,14 +12,17 @@ public:
         // try to extend the range [i, j]
 
         for (j = 0, i = 0; j < n; j++) {
-        	cout<<i<<" "<<j<<endl;
-            if (map.find(s[j]) !=map.end()) {
+        	cout<<i<<" "<<j<<" "<<s[j]<<endl;
+
+            if (map.count(s[j]) ) {
+            	//duplicate
                 i = max(map[s[j]], i);
             }
 
             ans = max(ans, j - i + 1);
             map[s[j+1]]= j + 1;
         }
+
         return ans;
     }
 };
