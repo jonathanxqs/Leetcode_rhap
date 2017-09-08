e3bcf3462e854ff7bafd5b3311494625f39083f8
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-08 15:20:39 -0400

    test1

--- 1.cpp
+++ 1.cpp
@@ -3,6 +3,8 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end() );
         std::map<int, int> maps;
+        vector<int> ans1;
+
         int i=0;
         for (auto s1: nums){
             maps[s1] = i;
@@ -10,12 +12,19 @@ public:
 
         }
 
-        int i=0;
+        i=0;
         for (auto s1: nums){
-            if ( maps[s1] ) = i;
+            if ( ( maps.find(target - s1) != maps.end() ) && maps[target - s1] != i ) {
+                int j = maps[target - s1];
+                cout << maps[s1] << j << endl;
+                ans1.push_back(i);
+                ans1.push_back(j);
+                return ans1;
+            }
             i++ ;
 
         }
-        
+
+        return ans1;
     }
 };
