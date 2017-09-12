adc3b51a959f0e5be29c43091f87ffdb40d5528a
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-12 16:21:54 -0400

    test1

--- 2.cpp
+++ 2.cpp
@@ -9,11 +9,13 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* l1r, *l2r,*p,*q;
-        
+        ListNode* l1r, *l2r,*p,*q;       
 
-        p = l1r , q =l2r;
-        ListNode *ret1 = new ListNode(0) , t1 ,t2;
+        p = l1 , q =l2;
+        ListNode *ret1 = new ListNode(0) , 
+        *t1=ret1 ,
+        *t2=ret1;
+
         int jw =0 ,sum1 = 0;
         while (p or q){
             if (p) sum1 += p->val;
@@ -22,8 +24,13 @@ public:
             jw = sum1 / 10;
             sum1 = sum1 % 10;
             t1 = new ListNode(sum1);
+            t2-> next = t1;
+            t2 = t1;
 
         }
+
+        t2->next = NULL;
+        return ret1->next;
 
 
 
