424e40b95086e679d2b20c466d0dbab17866e2d8
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-12 15:54:04 -0400

    no reverse

--- 2.cpp
+++ 2.cpp
@@ -10,34 +10,20 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* l1r, *l2r,*p,*q;
-        std::vector<ListNode *> v1,v2;
-        p = l1, q = l2;
-        while (p != nullptr) {
-            v1.push_back(p);
-            p = p->next;
-        }
         
 
-        while (q != nullptr) {
-            v2.push_back(q);
-            q = q->next;
-        }
-        
-        l1r = q;
-        for (auto p:v1.reverse()){
-            q -> next = p;
-            q = p;
-        }
-        q ->next = nullptr;
-        
-        l1r = l1r -> next;
+        p = l1r , q =l2r;
+        ListNode *ret1 = new ListNode(0) , t1 ,t2;
+        int jw =0 ,sum1 = 0;
+        while (p or q){
+            if (p) sum1 += p->val;
+            if (q) sum1 += q->val;
+            sum1 += jw;
+            jw = sum1 / 10;
+            sum1 = sum1 % 10;
+            t1 = new ListNode(sum1);
 
-        l1r = q;
-        for (auto p:v1.reverse()){
-            q -> next = p;
-            q = p;
         }
-        l1r = l1r -> next;
 
 
 
