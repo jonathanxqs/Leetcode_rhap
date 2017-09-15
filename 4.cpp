cc47fa1f41dc46154d93260f586e582b3f2a5d1b
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-14 23:59:07 -0400

    5

--- 4.cpp
+++ 4.cpp
@@ -2,15 +2,36 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         std::vector<int> v;
-        int i=0,j=0;
+        int i=0,j=0 , len_v = nums1.size() + nums2.size() ;
         while (i<nums1.size() or j< nums2.size() ){
+            if (i==nums1.size()) { 
+                v.push_back(nums2[j]);                
+                j++;
+                continue;            
+            }
+            
+            if (j==nums2.size()) {
+                v.push_back(nums1[i]);                
+                i++;
+                continue;     
+            }
+            
             if (nums1[i] <= nums2[j]){
-                
-                
+                v.push_back(nums1[i]);                
                 i++;
             }
+            else {
+                v.push_back(nums2[j]);                
+                j++;
+            }
+            
         }
-
+        
+        i = (len_v) / 2;
+        j =  ( (int) len_v -1 ) / 2;
+        //cout << i << " " << j <<endl;
+        
+        return (v[i]+v[j])/(double(2.0) );
 
     }
 };
