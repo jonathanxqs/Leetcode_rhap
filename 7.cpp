38beb677cded71757af5a917c0f8d37e807e44f3
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-16 19:26:35 -0400

    7

--- 7.cpp
+++ 7.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     int reverse(int x) {
-        
+        std::vector<int> v;
+        int y,i,j,sig;
+        long long k = x;
+        sig = (k>=0? 1:-1);
+        while (k != 0){
+            v.push_back(k % 10);
+            k = k /10;
+        }
+
+        k=0;
+        for (auto s = v.begin();s!=v.end();s++){
+            k = k *10 + *s;
+
+        }
+        k = k *  sig;
+        if (k > MAX_INT or k < MIN_INT) return 0;
+        return (int)k;
+
+
+
+
     }
 };
