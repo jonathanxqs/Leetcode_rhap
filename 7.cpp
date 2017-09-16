64add6bc44593d7617eef4a656e9ffbd51f4cec8
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-16 19:32:11 -0400

    ac7

--- 7.cpp
+++ 7.cpp
@@ -5,7 +5,9 @@ public:
         int y,i,j,sig;
         long long k = x;
         sig = (k>=0? 1:-1);
+
         while (k != 0){
+            cout<< k%10 << " ";
             v.push_back(k % 10);
             k = k /10;
         }
@@ -13,10 +15,10 @@ public:
         k=0;
         for (auto s = v.begin();s!=v.end();s++){
             k = k *10 + *s;
-
         }
-        k = k *  sig;
-        if (k > MAX_INT or k < MIN_INT) return 0;
+
+        // k = k *  sig;
+        if (k > INT_MAX or k < INT_MIN) return 0;
         return (int)k;
 
 
