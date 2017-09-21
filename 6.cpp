89d370e73af6ac9cbcf627efa8b8159c5beb4683
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-21 13:06:20 -0400

    6ac

--- 6.cpp
+++ 6.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
-    string convert(string s, int numRows) {
-        string s1,s2,s3;
+    string convert(string s, int nRows) {
+    
+    if (nRows <= 1)
+        return s;
 
-        int len_s = s.length();
-        int i=0,i1,i2,i3;
-        while(i<len_s){
-            if (i<len_s) { s1+=s[i];i++; }
-            if (i<len_s) { s2 +=s [i];i++; }
-            if (i<len_s) { s3+=s[i];i++ ;}
-            if (i<len_s) { s2+=s[i];i++; }
+    const int len = (int)s.length();
+    string *str = new string[nRows];
 
-        }
+    int row = 0, step = 1;
+    for (int i = 0; i < len; ++i)
+    {
+        str[row].push_back(s[i]);
 
-        return s1+s2+s3;
+        if (row == 0)
+            step = 1;
+        else if (row == nRows - 1)
+            step = -1;
+
+        row += step;
+    }
+
+    s.clear();
+    for (int j = 0; j < nRows; ++j)
+    {
+        s.append(str[j]);
+    }
+
+    delete[] str;
+    return s;
     }
 };
