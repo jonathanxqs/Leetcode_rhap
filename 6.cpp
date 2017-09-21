6895af8237057608c0ccfaf0dea2c65b0ecce79c
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-20 23:08:59 -0400

    7

--- 6.cpp
+++ 6.cpp
@@ -4,5 +4,15 @@ public:
         string s1,s2,s3;
 
         int len_s = s.length();
+        int i=0,i1,i2,i3;
+        while(i<len_s){
+            if (i<len_s) { s1+=s[i];i++; }
+            if (i<len_s) { s2 +=s [i];i++; }
+            if (i<len_s) { s3+=s[i];i++ ;}
+            if (i<len_s) { s2+=s[i];i++; }
+
+        }
+
+        return s1+s2+s3;
     }
 };
