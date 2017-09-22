ccfeedb804447733b45d80ce8078f81e73925986
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-22 15:23:44 -0400

    ac8

--- 8.cpp
+++ 8.cpp
@@ -2,9 +2,23 @@ class Solution {
 public:
     int myAtoi(string str) {
         int i,j,k;
-        long long rt_l;
+        long long rt_l = 0;
+        int sig =1 ;
         
         int len_s = str.length;
+        i = 0;
+        while (i<len_s && str[i]==' ') i++;
+        if (str[i] == '-') { sig = -1; i++; }
+        else if (str[i] == '+'){ sig = 1; i++;} 
+        
+        while (i<len_s and str[i]<='9' and str[i]>='0'){
+            rt_l *= 10;
+            rt_l += str[i]-'0';
+            i++;
+        }
+        
+        return rt_l;
+        
         
     }
 };
