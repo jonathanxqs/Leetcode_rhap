51a646e6627d5c63791046f994fd01d7e6ac2cd1
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-21 20:37:20 -0400

    ac 1

--- imc_1.cpp
+++ imc_1.cpp
@@ -8,10 +8,30 @@ int solution(string &S) {
     // write your code in C++14 (g++ 6.2.0)
     
     int len_s = S.size();
-    int i=0,j,k,l;
+    int i=0,j,k,left,rt_ans = -1;
+    bool upCase1 = false;
+    left = 0;
+    
     while (i<len_s){
-        
+        if(S[i] <='9' and S[i] >= '0'){
+            upCase1 = false;
+            
+            i++;
+            left=i;
+            continue;
+            }
+            
+        if(S[i] <='Z' and S[i] >= 'A'){
+            upCase1 = true;  
+            }
+            
+        if (i-left+1 > rt_ans and upCase1){
+                rt_ans = i-left+1;
+            }  
+        i++;
     }
+    
+    return rt_ans;
     
         
     
