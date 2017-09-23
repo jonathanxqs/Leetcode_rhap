b61de9feb7db015d1312841f3b8ac90c232267ca
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-23 19:43:33 -0400

    pivot

--- 215.cpp
+++ 215.cpp
@@ -108,3 +108,18 @@ public:
 };
 
 
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        multiset<int> mset;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) { 
+            mset.insert(nums[i]);
+            if (mset.size() > k)
+                mset.erase(mset.begin());
+        }
+        return *mset.begin();
+    }
+};
+
+
