a673de26852460031b9b43cd80bb6232495a2891
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-24 13:39:29 -0400

    21

---
+++ 21.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* p = l1;
+        ListNode* q = l2;
+        
+        ListNode* h = new ListNode(0);
+        ListNode* t = h;
+        
+        while (p!=null or q!=null){
+            
+        }
+        
+        
+        
+    }
+};
