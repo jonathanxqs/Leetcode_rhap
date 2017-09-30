c0647c827cb63a0b412f35bbb71957b8cd7f4de8
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-30 00:23:54 -0400

    18

--- 18.cpp
+++ 18.cpp
@@ -4,15 +4,15 @@ public:
         vector<vector<int>> total;
         int n = nums.size();
         if (n<4)  return total;
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());  // change orginal
         for(int i=0;i<n-3;i++)
         {
-            if(i>0&&nums[i]==nums[i-1]) continue;
-            if(nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target) break;
-            if(nums[i]+nums[n-3]+nums[n-2]+nums[n-1]<target) continue;
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            if(nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target) break; //impossible
+            if(nums[i]+nums[n-3]+nums[n-2]+nums[n-1]<target) continue;  // impossible
             for(int j=i+1;j<n-2;j++)
             {
-                if(j>i+1&&nums[j]==nums[j-1]) continue;
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
                 if(nums[i]+nums[j]+nums[j+1]+nums[j+2]>target) break;
                 if(nums[i]+nums[j]+nums[n-2]+nums[n-1]<target) continue;
                 int left=j+1,right=n-1;
@@ -22,7 +22,7 @@ public:
                     else if(sum>target) right--;
                     else{
                         total.push_back(vector<int>{nums[i],nums[j],nums[left],nums[right]});
-                        do{left++;}while(nums[left]==nums[left-1]&&left<right);
+                        do{left++;}while(nums[left]==nums[left-1]&&left<right); // avoid repeating
                         do{right--;}while(nums[right]==nums[right+1]&&left<right);
                     }
                 }
