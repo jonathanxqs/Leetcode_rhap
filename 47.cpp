06707798f9054857645e0697a24f4a86cc23c24e
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-30 17:14:03 -0400

    47

--- 47.cpp
+++ 47.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
 
-vector<vector<int> > permuteUnique(vector<int> &num) {
-    vector<vector<int>> v;
-    vector<int> r;
-    map<int, int> maps;
-    for (int i : num)
-    {
-        if (maps.find(i) == maps.end()) maps[i] = 0;
-        map[i]++;
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        vector<vector<int>> v;
+        vector<int> r;
+        map<int, int> maps;
+        for (int i : num)
+        {
+            if (maps.find(i) == maps.end()) maps[i] = 0;
+            maps[i]++;
+        }
+
+        permuteUnique(v, r, maps, num.size());
+        return v;
     }
 
-    permuteUnique(v, r, map, num.size());
-    return v;
-}
+    void permuteUnique(vector<vector<int>> &v, vector<int> &r, map<int, int> &map, int n){
+        if (n <= 0) //  r full
+        {
+            v.push_back(r);
+            return;
+        }
 
-void permuteUnique(vector<vector<int>> &v, vector<int> &r, map<int, int> &map, int n)
-{
-    if (n <= 0) //  r full
-    {
-        v.push_back(r);
-        return;
+        for (auto &p : map)
+        {
+            if (p.second <= 0) continue;
+            p.second--;
+            r.push_back(p.first);
+            permuteUnique(v, r, map, n - 1);
+            r.pop_back();
+            p.second++;
+        }
     }
-    
-    for (auto &p : map)
-    {
-        if (p.second <= 0) continue;
-        p.second--;
-        r.push_back(p.first);
-        permuteUnique(v, r, map, n - 1);
-        r.pop_back();
-        p.second++;
-    }
-}
 };
