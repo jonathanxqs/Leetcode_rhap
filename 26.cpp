7ee41060fdc3f726259e47e8b4b1aa43c32c03b8
Rhapsodistic <xucanxucan123@gmail.com>
2017-10-06 15:33:24 -0400

    26

---
+++ 26.cpp
@@ -0,0 +1,14 @@
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int count = 0;
+        for(int i = 1; i < n; i++){
+            if(A[i] == A[i-1]) count++;
+            else A[i-count] = A[i];
+        }
+        return n-count;
+        
+    }
+};
