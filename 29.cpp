110e5329da2b629c3c7f4af2e87dd8b4b28623fb
Rhapsodistic <xucanxucan123@gmail.com>
2017-10-07 16:30:42 -0400

    29c

--- 29.cpp
+++ 29.cpp
@@ -3,9 +3,11 @@ public:
     int divide(int dividend, int divisor) {
         if (!divisor || (dividend == INT_MIN && divisor == -1))
             return INT_MAX;
+        // long abssss
         int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
         long long dvd = labs(dividend);
         long long dvs = labs(divisor);
+
         int res = 0;
         while (dvd >= dvs) { 
             long long temp = dvs, multiple = 1;
