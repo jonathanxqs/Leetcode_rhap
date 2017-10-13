20b19e0ae6bf254852408903e50171177e056404
Rhapsodistic <xucanxucan123@gmail.com>
2017-10-12 21:35:37 -0400

    153

--- 153.cpp
+++ 153.cpp
@@ -3,9 +3,17 @@ public:
     int findMin(vector<int>& nums) {
         int l = 0 , r = nums.size();
         int mid = 0;
+
+        // 4 5 6 7m 8 0 1 2
         while (l>r){
             mid = (l+r)/2;
-            
+            if (nums[l]<nums[r]) return nums[l];
+
+            if (nums[mid]<nums[r])
+            l = mid;     
+            else r = mid;
         }
+
+        return nums[mid];
     }
 };
