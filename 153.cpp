b978bdecde76bbb0b37f4a1038c1b93f22acf693
Rhapsodistic <xucanxucan123@gmail.com>
2017-10-12 21:13:16 -0400

    153

---
+++ 153.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0 , r = nums.size();
+        int mid = 0;
+        while (l>r){
+            mid = (l+r)/2;
+            
+        }
+    }
+};
