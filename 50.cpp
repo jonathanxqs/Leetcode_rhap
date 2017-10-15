edb51b5a6c78731f77a0056009260640f318c4bd
Rhapsodistic <xucanxucan123@gmail.com>
2017-10-15 16:52:17 -0400

    50

--- 50.cpp
+++ 50.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n<0) return 1/x * myPow(1/x, -(n+1));
+        if(n<0) return 1/x * myPow(1/x, -(n+1)); // -n might overflow
         if(n==0) return 1;
         if(n==2) return x*x;
         if(n%2==0) return myPow( myPow(x, n/2), 2);
