44fe3edded9c28b39b71cb3db29983799cb4d002
Rhapsodistic <xucanxucan123@gmail.com>
2017-10-15 16:34:55 -0400

    73

--- 73.cpp
+++ 73.cpp
@@ -1,5 +1,3 @@
-
-
 class Solution {
 public:
     void setZeroes(vector<vector<int> > &matrix) {
@@ -9,7 +7,7 @@ public:
             if (matrix[i][0] == 0) col0 = 0;
             for (int j = 1; j < cols; j++)
                 if (matrix[i][j] == 0)
-                    matrix[i][0] = matrix[0][j] = 0;
+                    matrix[i][0] = matrix[0][j] = 0; //chain
         }
 
         for (int i = rows - 1; i >= 0; i--) {
@@ -18,5 +16,7 @@ public:
                     matrix[i][j] = 0;
             if (col0 == 0) matrix[i][0] = 0;
         }
+
+        // O 1 extra space
     }
 };
