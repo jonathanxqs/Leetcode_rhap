51d8b3d8b0309db072f4a199e13d1fb50e8b9990
Rhapsodistic <xucanxucan123@gmail.com>
2017-10-15 16:42:00 -0400

    42

---
+++ 42.cpp
@@ -0,0 +1,9 @@
+int trap(vector<int>& height) {
+    int l = 0, r = height.size()-1, level = 0, water = 0;
+    while (l < r) {
+        int lower = height[height[l] < height[r] ? l++ : r--];
+        level = max(level, lower);
+        water += level - lower;
+    }
+    return water;
+}
