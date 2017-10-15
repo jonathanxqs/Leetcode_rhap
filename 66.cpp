8d7d34262a3bc0ca45c0b5954f84be7e51682e56
Rhapsodistic <xucanxucan123@gmail.com>
2017-10-15 15:50:45 -0400

    66

---
+++ 66.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry1 = 1;
+        for (auto i=digits.rbegin();i != digits.rend();i++){
+            carry1 = ( *i + carry1 );
+            *i = carry1 % 10;
+            carry1 = carry1 /10;
+        }
+        if (carry1>0) digits.insert(digits.begin(),carry1);
+
+        return digits;
+
+
+    }
+};
