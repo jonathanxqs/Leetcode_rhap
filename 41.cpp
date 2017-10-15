17ce81bc88143a565da2fb5a7439771a217b592f
Rhapsodistic <xucanxucan123@gmail.com>
2017-10-15 15:43:40 -0400

    41

--- 41.cpp
+++ 41.cpp
@@ -1,14 +1,16 @@
 class Solution
 {
 public:
-    int firstMissingPositive(int A[], int n)
+    int firstMissingPositive(vector<int>& nums)
     {
+        int n = nums.size();
+
         for(int i = 0; i < n; ++ i)
-            while(A[i] > 0 && A[i] <= n && A[A[i] - 1] != A[i])
-                swap(A[i], A[A[i] - 1]);
-        
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
+                swap(nums[i], nums[nums[i] - 1]);
+        //time O(n) solution
         for(int i = 0; i < n; ++ i)
-            if(A[i] != i + 1)
+            if(nums[i] != i + 1)
                 return i + 1;
         
         return n + 1;
