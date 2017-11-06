9607518543c38a15258461dbb1753b9f32dec10e
Rhapsodistic <xucanxucan123@gmail.com>
2017-11-06 16:47:37 -0500

    cg 75

--- 75.cpp
+++ 75.cpp
@@ -3,11 +3,12 @@ public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
         int second=n-1, zero=0;
-        for (int i=0; i<=second; i++) {
-            while (nums[i]==2 && i<second) swap(nums[i], nums[second--]);
-            while (nums[i]==0 && i>zero) swap(nums[i], nums[zero++]);
+        for (int i=0; i<n; i++) {
+            while (nums[i]==2 && i<=second) swap(nums[i], nums[second--]);
+            while (nums[i]==0 && i>=zero ) swap(nums[i], nums[zero++]);
         }
     }
 };
 
 // 2 time counting sort is not best
+// 2 last , 0 first , 1 mid
