43cbed244e649aee75ff581697e3b7b21a99335b
Rhapsodistic <xucanxucan123@gmail.com>
2017-12-09 18:21:29 -0500

    g216

--- 216.cpp
+++ 216.cpp
@@ -8,13 +8,16 @@ public:
     }
     
 private:
-    void combinationSum3(int target, std::vector<std::vector<int> > &res, std::vector<int> &combination, int begin, int need) {
+    void combinationSum3(int target, std::vector<std::vector<int> > &res,const std::vector<int> &combination, int begin, int need) {
+        
+        // okay , target == 0
         if (!target) {
             res.push_back(combination);
             return;
         }
-        else if (!need)
+        else if (!need) // need == 0 , end
             return;
+
         for (int i = begin; i != 10 && target >= i * need + need * (need - 1) / 2; ++i) {
             combination.push_back(i);
             combinationSum3(target - i, res, combination, i + 1, need - 1);
