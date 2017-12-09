294674f540b3abdef85b2e2d91ea022411a6e2a3
Rhapsodistic <xucanxucan123@gmail.com>
2017-12-09 17:11:38 -0500

    39

--- 39.cpp
+++ 39.cpp
@@ -4,17 +4,25 @@ public:
         std::sort(candidates.begin(), candidates.end());
         std::vector<std::vector<int> > res;
         std::vector<int> combination;
+
         combinationSum(candidates, target, res, combination, 0);
         return res;
     }
-    
+
 private:
-    void combinationSum(std::vector<int> &candidates, int target, std::vector<std::vector<int> > &res, std::vector<int> &combination, int begin) {
+
+    /*
+        @param : candidates , res return , combination already in , begin start point
+         
+     */
+    void combinationSum(const std::vector<int> &candidates , int target, std::vector<std::vector<int> > &res, std::vector<int> &combination, int begin) {
+
         if (!target) {
             res.push_back(combination);
             return;
         }
-        for (int i = begin; i != candidates.size() && target >= candidates[i]; ++i) {
+
+        for (int i = begin; i != candidates.size() && target >= candidates[i]; ++i) { 
             combination.push_back(candidates[i]);
             combinationSum(candidates, target - candidates[i], res, combination, i);
             combination.pop_back();
