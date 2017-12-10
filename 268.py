ff2eca86ea67882f7652b65c9359c9935816a07e
Rhapsodistic <xucanxucan123@gmail.com>
2017-12-09 19:22:52 -0500

    mine ac

--- 268.py
+++ 268.py
@@ -8,15 +8,24 @@ class Solution:
         n = len(nums) + 1
         nums = ( list ( map(lambda x: x+1 , nums) )) ;
         print(nums)        
-        nums.append(0);  
+        nums.append(n+2);  
 
         for i in range(n-1):
-            nums[abs(i)-1] *= -1;
+            nums[abs(nums[i]) - 1] *= -1;
 
         print(nums)
 
+        nums[n-1] = n
         for i,j in enumerate(nums):
-            print(i,j)
+            #print(i,j)
             if j>0:
                 return i+1-1;
+
+
+
+    def missingNumber(self, nums):
+        n = len(nums)
+        return n * (n+1) / 2 - sum(nums)
+
+
 
