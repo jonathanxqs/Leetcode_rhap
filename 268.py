aeb8322caad7b68af802021e93c6ddaf2257d93f
Rhapsodistic <xucanxucan123@gmail.com>
2017-12-09 19:13:44 -0500

    constant

--- 268.py
+++ 268.py
@@ -5,16 +5,18 @@ class Solution:
         :rtype: int
         """
         
-        n = len(nums) + 1;
-        nums.append(n);
-        map(lambda x:x+1,nums)
+        n = len(nums) + 1
+        nums = ( list ( map(lambda x: x+1 , nums) )) ;
+        print(nums)        
+        nums.append(0);  
 
-
-        for i in range(n):
+        for i in range(n-1):
             nums[abs(i)-1] *= -1;
 
+        print(nums)
 
         for i,j in enumerate(nums):
+            print(i,j)
             if j>0:
                 return i+1-1;
 
